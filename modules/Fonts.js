994a4eb215c67645e4afa8fa2f363624d57174c8
Shawn Dibble <shawndibble@gmail.com>
2020-08-15 12:05:00 -0500

    Update Fonts.js

--- modules/Fonts.js
+++ modules/Fonts.js
@@ -28,6 +28,8 @@ export default class Fonts {
     fontEl.attr('href', `https://fonts.googleapis.com/css2?${fonts}&display=swap`);
     $('head').append(fontEl);
     fontEl.on('load', () => {
+      // Try to redraw drawings. If the font isn't loaded. Then wait 5 seconds and try again.
+      this.drawDrawings()
       setTimeout(() => this.drawDrawings(), 5000);
       if (options.settings) {
         ui.notifications.info(game.i18n.localize('ForienCustomFonts.Notifications.FontAdded'), {permanent: true});
