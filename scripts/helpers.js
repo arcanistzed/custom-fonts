8f8e11cdcd01b24710edd0345ca4857c6229d858
arcanist <arcanistzed@gmail.com>
2022-01-07 20:35:37 -0500

    fix: limit recursion for getting font subfolders
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,11 +1,15 @@
 /** Recursively browse for all files in a directory including subdirectories
  * @param {string} directory The directory to browse
+ * @param {string} l The initial recursion level
  * @return {Promise<string[]>} All of the files in the directory and it's subdirectories
  */
-export async function recursiveFileBrowse(directory) {
+export async function recursiveFileBrowse(directory, l = 0) {
+    if (l >= 50) return [];
     const res = await FilePicker.browse("data", directory);
     const files = res.files;
-    for (const dir of res.dirs)
-        files.push(...await recursiveFileBrowse(dir));
+    for (const dir of res.dirs) {
+        files.push(...await recursiveFileBrowse(dir, l));
+        l++;
+    };
     return files;
 };
